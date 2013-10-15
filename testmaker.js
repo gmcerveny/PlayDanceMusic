8752a0abbd2cc7b6d07df7b0cc2306f38df464f8
Greg Cerveny <greg.cerveny@gmail.com>
2013-10-14 19:14:11 -0500

    making index render inline

--- testmaker.js
+++ testmaker.js
@@ -3,23 +3,12 @@ var ejs = require('ejs'),
   yamlFM = require('yaml-front-matter'),
   namp = require('namp');
 
-var template_name = 'index';
-var tests = ['no_price', 'premium', 'ultra'];
-var template_path = path.resolve(template_name + '.ejs');
-
-function renderTests() {   
-    tests.forEach( function(test) {
-        var options = new Object();
-        options[test] = true;
-        output_name = template_name + '_' + test + '.html';
-        output_path = path.resolve('.', output_name);
-        renderTest(template_path, options, output_path);
-    });
-}
-
-function renderControl() {
-    output_path = path.resolve('.', template_name + '.html');
-    renderTemplate(template_path, {control: true}, output_path);
+function quickRender(quick_template, options) {
+    var basename = path.basename(quick_template, '.ejs');
+    var quick_template_path = path.resolve('.', basename + '.ejs');
+    var quick_output_path = path.resolve('.', basename + '.html');
+    options = options || {};
+    renderTemplate(quick_template_path, options, quick_output_path);
 }
 
 function renderTemplate(template_path, options, output_path) {
@@ -32,14 +21,6 @@ function renderTemplate(template_path, options, output_path) {
                                         console.log( path.basename(output_path) + ' written');
                                     });
                    });
-}
-
-function quickRender(quick_template, options) {
-    var basename = path.basename(quick_template, '.ejs');
-    var quick_template_path = path.resolve('.', basename + '.ejs');
-    var quick_output_path = path.resolve('.', basename + '.html');
-    options = options || {};
-    renderTemplate(quick_template_path, options, quick_output_path);
 }
 
 function getSlug(title, modifier) {
